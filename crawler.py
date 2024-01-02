ccdb5351e72b542f0c1f6a3424adbcbd30fb7c92
yeonjin1357 <yeonjin1357@naver.com>
2024-01-02 18:19:57 +0900

    테스트

--- crawler.py
+++ crawler.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup as bs
 from lxml import etree
 
 URL = "https://velog.io/@yeonjin1357"
-ARTICLE_SELECTOR = '//*[contains(@class, "FlatPostCardList_block")]'
+# 게시글을 나타내는 더 구체적인 클래스 이름으로 ARTICLE_SELECTOR 수정
+ARTICLE_SELECTOR = '//*[contains(@class, "FlatPostCard_block__")]'
 
 class Crawler:
     def __init__(self):
@@ -19,67 +20,53 @@ class Crawler:
 htmlparser = etree.HTMLParser()
 
 class Article:
-    href: str
-    headline: str
-    context: str
-    date: str
-    tags: list[str]
-    thumbnail: str
+    def __init__(self, elem: etree._Element):
+        # XPath 쿼리를 현재 게시물의 HTML 구조에 맞게 수정
+        self.href = elem.xpath(".//a/@href")[0]
+        self.headline = elem.xpath(".//h2/text()")[0].strip()
+        self.context = elem.xpath(".//p/text()")[0].strip()
+        # 날짜 추출을 위한 XPath 수정
+        self.date = elem.xpath(".//div[contains(@class, 'FlatPostCard_subInfo__')]/span[1]/text()")[0].strip()
+        # 썸네일 이미지 URL 추출을 위한 XPath 수정
+        self.thumbnail = elem.xpath(".//img/@src")[0].strip()
+        self.tags = self.get_tags(elem)
 
     def to_dict(self):
-        data = {}
-        data.update(
-            href=str(self.href),
-            headline=str(self.headline),
-            context=str(self.context),
-            date=str(self.date),
-            tags=str(self.tags),
-            thumbnail=str(self.thumbnail)
-        )
-        return data
-
-    def __init__(self, elem: etree._Element):
-        head: list[etree._Element] = elem.xpath("a[2]")
-        if not head:
-            head = elem.xpath("a[1]")
-        date = elem.xpath("div[2]/span")[0].text
-        context = elem.xpath("p")[0].text
-        thumbnail = elem.xpath("a[1]/div/img/@src")[0]
-        self.href = head[0].attrib.get("href")
-        self.headline = head[0].xpath("h2")[0].text
-        self.context = context
-        self.date = date
-        self.tags = self.get_tags(elem)
-        self.thumbnail = thumbnail
+        # 모든 속성을 사전으로 변환
+        return {
+            'href': self.href,
+            'headline': self.headline,
+            'context': self.context,
+            'date': self.date,
+            'tags': self.tags,
+            'thumbnail': self.thumbnail
+        }
 
     def get_tags(self, elem: etree._Element):
-        tag_str: list[str] = []
-        tags = elem.xpath("div[1]")
-        for _tags in tags:
-            for tag in _tags:
-                tag_str.append(tag.text)
-        return tag_str
+        # 태그 추출을 위한 XPath 수정
+        tag_elements = elem.xpath(".//div[contains(@class, 'FlatPostCard_tagsWrapper__')]/a")
+        return [tag.text.strip() for tag in tag_elements]
 
 def get_articles():
-    articles: list[Article] = []
+    articles = []
     crawler = Crawler()
     req = crawler.get_page(URL)
     parsed = crawler.parse(req)
     dom = etree.HTML(str(parsed), htmlparser)
-    elems: list[etree._Element] = dom.xpath(ARTICLE_SELECTOR)
+    elems = dom.xpath(ARTICLE_SELECTOR)
     for elem in elems:
-        articles.append(Article(elem))
+        # 예외 처리를 추가하여 빈 요소로 인한 오류 방지
+        try:
+            articles.append(Article(elem))
+        except IndexError as e:
+            print(f"Error processing element: {e}")
     return articles
 
-def to_json(data):
-    with open('articles.json','w+',encoding="utf-8",) as file:
-            print(data)
-            file.write(json.dumps(data, indent=4,ensure_ascii=False))
+def to_json(data, filename='articles.json'):
+    with open(filename, 'w+', encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
-    articles = get_articles()
-    article_data = []
-    for article in articles:
-        article_data.append(article.to_dict())
-    to_json(article_data)
-
+    articles_data = get_articles()
+    articles_dict = [article.to_dict() for article in articles_data]
+    to_json(articles_dict)

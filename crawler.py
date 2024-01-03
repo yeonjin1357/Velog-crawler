cb3ed51661de12fc180541c7393272b6b84c1abf
yeonjin1357 <yeonjin1357@naver.com>
2024-01-03 09:56:04 +0900

    수정16

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup as bs
 from lxml import etree
 
 URL = "https://velog.io/@yeonjin1357"
-ARTICLE_SELECTOR = '//*[@id="root"]/div[2]/div[3]/div[4]/div[3]'
+ARTICLE_SELECTOR = '//*[@id="html"]/body/div/div[1]/div[2]/main/section/div[2]/div[2]'
 
 class Crawler:
     def __init__(self):
@@ -25,61 +25,48 @@ class Article:
     date: str
     tags: list[str]
     thumbnail: str
+    comments: int
+    hearts: int
 
     def to_dict(self):
-        data = {}
-        data.update(
-            href=str(self.href),
-            headline=str(self.headline),
-            context=str(self.context),
-            date=str(self.date),
-            tags=str(self.tags),
-            thumbnail=str(self.thumbnail)
-        )
+        data = {
+            "href": self.href,
+            "headline": self.headline,
+            "context": self.context,
+            "date": self.date,
+            "tags": self.tags,
+            "thumbnail": self.thumbnail,
+            "comments": self.comments,
+            "hearts": self.hearts
+        }
         return data
 
     def __init__(self, elem: etree._Element):
-        head: list[etree._Element] = elem.xpath("a[2]")
-        if not head:
-            head = elem.xpath("a[1]")
-        date = elem.xpath("div[2]/span")[0].text
-        context = elem.xpath("p")[0].text
-        thumbnail = elem.xpath("a[1]/div/img/@src")[0]
-        self.href = head[0].attrib.get("href")
-        self.headline = head[0].xpath("h2")[0].text
-        self.context = context
-        self.date = date
-        self.tags = self.get_tags(elem)
-        self.thumbnail = thumbnail
-
-    def get_tags(self, elem: etree._Element):
-        tag_str: list[str] = []
-        tags = elem.xpath("div[1]")
-        for _tags in tags:
-            for tag in _tags:
-                tag_str.append(tag.text)
-        return tag_str
+        self.href = elem.xpath("a[1]/@href")[0]
+        self.thumbnail = elem.xpath("a[1]/img/@src")[0]
+        self.headline = elem.xpath("a[2]/h2/text()")[0]
+        self.context = elem.xpath("p/text()")[0]
+        self.date = elem.xpath("div[2]/span/text()")[0]
+        self.tags = [tag.text for tag in elem.xpath("div[1]/a")]
+        self.comments = int(elem.xpath("div[2]/div/span[1]/text()")[0])
+        self.hearts = int(elem.xpath("div[2]/div/span[3]/text()")[0])
 
 def get_articles():
-    articles: list[Article] = []
+    articles = []
     crawler = Crawler()
     req = crawler.get_page(URL)
     parsed = crawler.parse(req)
     dom = etree.HTML(str(parsed), htmlparser)
-    elems: list[etree._Element] = dom.xpath(ARTICLE_SELECTOR)
+    elems = dom.xpath(ARTICLE_SELECTOR)
     for elem in elems:
         articles.append(Article(elem))
     return articles
 
 def to_json(data):
-    with open('articles.json','w+',encoding="utf-8",) as file:
-            print(data)
-            file.write(json.dumps(data, indent=4,ensure_ascii=False))
+    with open('articles.json', 'w+', encoding="utf-8") as file:
+        file.write(json.dumps(data, indent=4, ensure_ascii=False))
 
 if __name__ == "__main__":
     articles = get_articles()
-    article_data = []
-    for article in articles:
-        article_data.append(article.to_dict())
+    article_data = [article.to_dict() for article in articles]
     to_json(article_data)
-

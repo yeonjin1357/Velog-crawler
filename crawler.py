5ccb49f2163255ac5488260f096c667ab1def17d
yeonjin1357 <yeonjin1357@naver.com>
2024-01-03 09:02:36 +0900

    수정12

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup as bs
 from lxml import etree
 
 URL = "https://velog.io/@yeonjin1357"
-ARTICLE_SELECTOR = '//*[@id="html"]/body/div/div[1]/div[2]/main/section/div[2]/div[2]/div[1]'
+ARTICLE_SELECTOR = '//*[@id="html"]/body/div/div[1]/div[2]/main/section/div[2]/div[2]'
 
 class Crawler:
     def __init__(self):

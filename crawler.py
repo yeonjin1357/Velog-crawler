f648e33e2dfd4fbb5980cf5978b669eee29a6c96
yeonjin1357 <yeonjin1357@naver.com>
2024-01-03 09:40:09 +0900

    수정15

--- crawler.py
+++ crawler.py
@@ -19,44 +19,67 @@ class Crawler:
 htmlparser = etree.HTMLParser()
 
 class Article:
-    def __init__(self, elem: etree._Element):
-        self.href = elem.xpath(".//a[1]/@href")[0]
-        self.headline = elem.xpath(".//h2/text()")[0].strip()
-        self.context = elem.xpath(".//p/text()")[0].strip()
-        self.date = elem.xpath(".//div[contains(@class, 'FlatPostCard_subInfo__')]/span[1]/text()")[0].strip()
-        self.thumbnail = elem.xpath(".//a[1]/div/img/@src")[0].strip()
-        self.tags = self.get_tags(elem)
+    href: str
+    headline: str
+    context: str
+    date: str
+    tags: list[str]
+    thumbnail: str
 
     def to_dict(self):
-        return {
-            'href': self.href,
-            'headline': self.headline,
-            'context': self.context,
-            'date': self.date,
-            'tags': self.tags,
-            'thumbnail': self.thumbnail
-        }
+        data = {}
+        data.update(
+            href=str(self.href),
+            headline=str(self.headline),
+            context=str(self.context),
+            date=str(self.date),
+            tags=str(self.tags),
+            thumbnail=str(self.thumbnail)
+        )
+        return data
+
+    def __init__(self, elem: etree._Element):
+        head: list[etree._Element] = elem.xpath("a[2]")
+        if not head:
+            head = elem.xpath("a[1]")
+        date = elem.xpath("div[2]/span")[0].text
+        context = elem.xpath("p")[0].text
+        thumbnail = elem.xpath("a[1]/div/img/@src")[0]
+        self.href = head[0].attrib.get("href")
+        self.headline = head[0].xpath("h2")[0].text
+        self.context = context
+        self.date = date
+        self.tags = self.get_tags(elem)
+        self.thumbnail = thumbnail
 
     def get_tags(self, elem: etree._Element):
-        tag_elements = elem.xpath(".//div[contains(@class, 'FlatPostCard_tagsWrapper__')]/a")
-        return [tag.text.strip() for tag in tag_elements]
+        tag_str: list[str] = []
+        tags = elem.xpath("div[1]")
+        for _tags in tags:
+            for tag in _tags:
+                tag_str.append(tag.text)
+        return tag_str
 
 def get_articles():
-    articles = []
+    articles: list[Article] = []
     crawler = Crawler()
     req = crawler.get_page(URL)
     parsed = crawler.parse(req)
     dom = etree.HTML(str(parsed), htmlparser)
-    elems = dom.xpath(ARTICLE_SELECTOR)
+    elems: list[etree._Element] = dom.xpath(ARTICLE_SELECTOR)
     for elem in elems:
         articles.append(Article(elem))
     return articles
 
-def to_json(data, filename='articles.json'):
-    with open(filename, 'w+', encoding="utf-8") as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)
+def to_json(data):
+    with open('articles.json','w+',encoding="utf-8",) as file:
+            print(data)
+            file.write(json.dumps(data, indent=4,ensure_ascii=False))
 
 if __name__ == "__main__":
-    articles_data = get_articles()
-    article_data = [article.to_dict() for article in articles_data]
+    articles = get_articles()
+    article_data = []
+    for article in articles:
+        article_data.append(article.to_dict())
     to_json(article_data)
+
